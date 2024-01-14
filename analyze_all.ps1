cf1312d21ae4d79beb7bde02fd2c31c92622bdf4
estshorter <1430311+estshorter@users.noreply.github.com>
2024-01-14 23:46:00 +0900

    [feat] 全実行用のps1スクリプト追加

---
+++ analyze_all.ps1
@@ -0,0 +1,4 @@
+python analyzer.py onboard && 
+python analyzer.py fishing && 
+python analyzer.py biim && 
+python analyzer.py kitchen
---
+++ gett_all.ps1
@@ -0,0 +1,4 @@
+python getter.py onboard && 
+python getter.py fishing && 
+python getter.py biim && 
+python getter.py kitchen
